53f21b74dcee6400cd95167b4be7b147ea2822cc
pizzaminded <pizzaminded@users.noreply.github.com>
2020-05-01 23:46:46 +0200

    [Security] move auth exceptions to new namespace

---
+++ Exception/InvalidCredentialsException.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Security\Auth\Exception;
+
+/**
+ * @author  pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class InvalidCredentialsException extends \Error
+{
+    /**
+     * Things passed by used, extracted in
+     *
+     * @var mixed
+     */
+    protected $passedCredentials;
+
+    /**
+     * @param mixed $passedCredentials
+     *
+     * @return void
+     */
+    public function setPassedCredentials($passedCredentials): void
+    {
+        $this->passedCredentials = $passedCredentials;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassedCredentials()
+    {
+        return $this->passedCredentials;
+    }
+}
---
+++ Exception/UserNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Security\Auth\Exception;
+
+/**
+ * Class UserNotFoundException
+ *
+ * @package Jadob\Security\Exception
+ * @author  pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class UserNotFoundException extends \Error
+{
+
+}
